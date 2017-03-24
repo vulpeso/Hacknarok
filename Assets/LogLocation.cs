088fa99d299f95e020664a4bb5371b56216d22f8
Arkadiusz Lis <vulpes.lis@gmail.com>
2017-03-24 23:17:12 +0100

    no message

---
+++ Assets/LogLocation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogLocation : MonoBehaviour {
+	LocationInfo li = new LocationInfo();
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Twoja lokacja to: " + li.latitude.ToString("0.00") +" "+ li.longitude.ToString("0.00")+"\n");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
